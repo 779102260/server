6011c52dd834910d430a7ce7f1c71f225a79fbf2
cc <y779102260@gmail.com>
2023-12-23 15:50:00 +0800

    publish

--- packages/h3/CHANGELOG.md
+++ packages/h3/CHANGELOG.md
@@ -1,11 +1,5 @@
-## 1.0.4
 
 ## 1.0.4
-
-### Patch Changes
-
-- 测试
-- 修改文档
 
 ### Patch Changes
 
---
+++ publish.sh
@@ -0,0 +1,23 @@
+# 询问是否进入预发布模式
+read -p "是否进入预发布模式? [y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    changeset pre enter
+fi
+
+# 修改版本号
+changeset version
+
+# 询问确认发布
+read -p "确认发布? [y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    changeset publish
+fi
+
+# 提交
+git add .
+git commit -m "publish"
+git push
